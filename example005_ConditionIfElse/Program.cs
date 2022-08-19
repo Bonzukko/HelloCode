dff046372efa5df764ea04fec0e2905358ffd5aa
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-08-19 22:57:06 +0300

    ex005

---
+++ example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") // команда .ToLower() - переводит все символы в нижний регистр, и Машу будет приветствовать вне зависимости от регистра написания любой из букв в этом имени, хоть маША
+
+{
+    Console.WriteLine("Ура! Это же МАША!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
