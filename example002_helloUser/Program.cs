61f47eab41670e0a0a08f9c17325a13f8774c362
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-18 23:14:00 +0300

    ex002

--- example002_helloUser/Program.cs
+++ example002_helloUser/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Write("Введите ваше имя ");
-string username = Console.ReadLine();
+﻿Console.Write("Введите ваше имя: "); // ; - ставим в конце любой операции
+string username = Console.ReadLine(); // данные будут стррочными (string - строка) знак = это не равно, а присваивание
+// ReadLine(); - Считать строку из терминала
 Console.Write("Hello, ");
-Console.Write(username);
+Console.Write(username); // Console.Write - выводит в строку, а Console.WriteLine каждый раз с новой строки
