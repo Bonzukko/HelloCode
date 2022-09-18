dc36af7eb0f46f2ec6a5d26bf799149ddf7950e4
Dmitry Fedotov <fedotov-ds1989@yandex.ru>
2022-09-18 23:14:56 +0300

    ex007

--- example007_Magic/Program.cs
+++ example007_Magic/Program.cs
@@ -1,10 +1,23 @@
-﻿Console.Clear();
+﻿                                // ФРАКТАЛЬНОЕ ИЗОБРАЖЕНИЕ - ТРЕУГОЛЬНИК СЕРПИНСКОГО
+
+// Агоритм:
+// 1. найти 3 точки
+// 2. выбрать 2 любых
+// 3. найти середину
+// 4. поставить точку
+// 5. выбрать случайную вершину треугольника
+// 6. соединить её с полученной на 4 шаге точкой
+// 7. перейти к шагу 3
+// 8. шаги 3 - 7 повторить 9, 28, 31 раз
+
+
+Console.Clear();
 // Console.SetCursorPosition(10,4);
 // Console.WriteLine("+");
 
-int xa = 30, ya = 1,
+int xa = 40, ya = 1,
     xb = 1, yb = 30,
-    xc = 60, yc = 30;
+    xc = 80, yc = 30;
 
 Console.SetCursorPosition(xa,ya);
 Console.WriteLine("+");
@@ -19,7 +32,7 @@ int x = xa, y = xb;
 
 int count = 0;
 
-while(count < 10)
+while(count < 1000)
 {
     int what = new Random(). Next(0,3);//[0,3) 0 1 2
     if  (what == 0 )
